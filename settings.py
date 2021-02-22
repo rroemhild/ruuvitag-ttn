3be7001a7de29045a6e3af8fd9ac249272b6a93b
Rafael Römhild <rafael@roemhild.de>
2021-02-22 11:10:37 +0100

    Update settings
    * RuuviTag whitelist is now a list instead of tuple. So we will use this later
      to add new mac addresses i.e. via downlink
    * Device deepsleap is now 5 minutes (300 sec) by default
    * Disable debug by default

--- settings.py
+++ settings.py
@@ -1,15 +1,19 @@
-DEBUG = True
+# Disable deepsleep
+DEBUG = False
 
 # OTAA activation
 NODE_APP_EUI = ''
 NODE_APP_KEY = ''
 
 # Device deepsleep time in seconds
-NODE_DEEPSLEEP = 30
-
-# RuuviTags whitelist, other tags will be ignored, all lowercase
-# RUUVITAGS = (b'aa:bb:02:03:04:05', b'aa:ab:12:13:14:15')
-RUUVITAGS = (b'c2:0d:d5:a3:b3:54')
+NODE_DEEPSLEEP = 300
 
 # Bluetooth scan timeout
 TIMEOUT = 10
+
+# RuuviTags whitelist, other tags will be ignored, all lowercase
+# RUUVITAGS = [b'aa:bb:02:03:04:05', b'aa:ab:12:13:14:15']
+RUUVITAGS = [
+    b'fc:e7:e3:e6:2f:0c',  # outdoor
+    b'c2:0d:d5:a3:b3:54'   # livingroom
+]
